01c81fef7b4771b6575fae9f1991c0468ec93426
Viola Bergere <viola.bergere@gmail.com>
2024-01-22 19:38:38 +0000

    Add title and author to admin site for posts

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"The title of this post is '{self.title}' | written by {self.author}"
+
 
 # Comment model
 class Comment(models.Model):
