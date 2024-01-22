1e6f22b91b6472e0ea5969ccfd1bfde024b0a5ae
Viola Bergere <viola.bergere@gmail.com>
2024-01-22 16:55:06 +0000

    Create Comment model

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 # Create your models here.
+# Post model
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -15,3 +16,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+
+# Comment model
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
