0182761ae8b50997a6c54f3d2adcdc09cf2abf63
Viola Bergere <viola.bergere@gmail.com>
2024-01-23 13:13:29 +0000

    Add views and post_list.html

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-violaberg-djangoblog-7jqz3e3g2sm.ws-eu107.gitpod.io',
                  '.herokuapp.com']
