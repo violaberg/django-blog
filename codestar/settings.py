3e75dbcd3617d29392b7c3d97757c66469ae0ab1
Viola Bergere <viola.bergere@gmail.com>
2024-01-24 12:42:12 +0000

    Install summernote

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-violaberg-djangoblog-7jqz3e3g2sm.ws-eu107.gitpod.io',
                  '.herokuapp.com']
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
