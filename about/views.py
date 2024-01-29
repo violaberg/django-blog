7321908cf9d19cb3d80a31f2a8e172b593ed5f47
Viola Bergere <viola.bergere@gmail.com>
2024-01-29 12:31:27 +0000

    Add docstrings and clean up code

--- about/views.py
+++ about/views.py
@@ -7,16 +7,36 @@ from django.contrib import messages
 # Create your views here.
 def about_me(request):
     """
-    Renders about page
+    Renders the most recent information on the website author
+    and allows user collaboration requests.
+
+    Displays an individual instance of :model:`about.About`.
+
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+        ``collaborate_form``
+            An instance of :form:`about.CollaborateForm`.
+    
+    **Template**
+    :template:`about/about.html`
     """
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
-            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                """Collaboration request received!
+                I endeavour to respond within 2 working days.""")
 
     about = About.objects.all().order_by('updated_on').first()
     collaborate_form = CollaborateForm()
 
-    return render(request, "about/about.html", {"about": about, "collaborate_form": collaborate_form},
+    return render(
+        request,
+        "about/about.html",
+        {"about": about,
+         "collaborate_form": collaborate_form},
     )
