011e719a4b099078aba4996fe4496847674fdcc8
Viola Bergere <viola.bergere@gmail.com>
2024-01-29 12:43:48 +0000

    Fix typo in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import django.contrib.messages import constants as messages
+from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
